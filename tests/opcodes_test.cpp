c32ca9b11f99e66ded0ef1185149061e1e1a1b96
cavelazquez8 <cavelazquez8@gmail.com>
2024-03-27 10:53:45 -0700

    ANNTest mod 1

--- tests/opcodes_test.cpp
+++ tests/opcodes_test.cpp
@@ -4,16 +4,31 @@
 
 namespace {
 
-TEST(OpcodesTest, ValidANNN) {
+TEST(ANNNTest, Valid) {
 
   Chip8 chip8;
 
-  chip8.setMem(0x200, 0xA0);
-  chip8.setMem(0x201, 0x01);
+  chip8.setMemory(0x200, 0xA0);
+  chip8.setMemory(0x201, 0x01);
 
   chip8.emulateCycle();
 
-  ASSERT_EQ(chip8.getIndexRegister(), 0x0001);
+  EXPECT_EQ(chip8.getIndexRegister(), 0x0001);
+}
+
+TEST(_00EETest, Valid) {
+  Chip8 chip8;
+
+  chip8.setStackPointer(2);
+  chip8.setStack(1, 0x300);
+
+  chip8.setMemory(0x200, 0x00);
+  chip8.setMemory(0x201, 0xEE);
+
+  chip8.emulateCycle();
+
+  EXPECT_EQ(chip8.getProgramCounter(), 0x302);
+  EXPECT_EQ(chip8.getStackPointer(), 1);
 }
 
 } // namespace
