a3a23424ec576e0d6132cb151aa0d2fe623eaca6
cavelazquez8 <cavelazquez8@gmail.com>
2024-04-28 20:08:21 -0700

    9XY0 implem & tests, chip8.h beautify

--- src/chip8.h
+++ src/chip8.h
@@ -30,20 +30,14 @@ public:
 
 private:
   std::uint8_t memory[4096];
-
   std::uint8_t registers[16];
   std::uint16_t indexRegister;
-
   std::uint16_t stack[16];
   std::uint8_t stackPointer;
-
   std::uint8_t delayTimer;
   std::uint8_t soundTimer;
-
   std::uint16_t programCounter;
-
   std::uint16_t opcode;
-
   bool drawFlag;
 };
 #endif
