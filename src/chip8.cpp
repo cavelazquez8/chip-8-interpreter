4f2cbf66b5b9b472137e30349eb5d6efebb230ea
cavelazquez8 <cavelazquez8@gmail.com>
2024-05-21 09:03:49 -0700

    FX55 implem & test

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -405,6 +405,15 @@ void Chip8::emulateCycle() {
 
       programCounter += 2;
     } break;
+    case 0x0055: {
+
+      std::uint8_t Vx = registers[(opcode & 0x0F00) >> 8];
+
+      for (int i = 0; i <= Vx; ++i) {
+        memory[indexRegister + i] = registers[i];
+      }
+      programCounter += 2;
+    }
     default:
       printf("Unknown Opcode: %X", opcode);
       break;
