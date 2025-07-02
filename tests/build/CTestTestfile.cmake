0008e26696ae41ba5a629632bede52d2d5282441
Carlos Alberto Velazquez Medina <83848862+cavelazquez8@users.noreply.github.com>
2025-07-01 17:41:26 -0700

    Delete tests/build directory

--- tests/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cavelazquez8/dev/projects/chip-8-interpreter/tests
-# Build directory: /home/cavelazquez8/dev/projects/chip-8-interpreter/tests/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/cavelazquez8/dev/projects/chip-8-interpreter/tests/build/tests[1]_include.cmake")
-subdirs("_deps/googletest-build")
